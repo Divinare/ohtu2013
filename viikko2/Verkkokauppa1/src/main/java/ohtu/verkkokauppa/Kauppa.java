be33000d547d5adfc33f59d6d0411336c30101cf
Joe <niemijoe@hotmail.com>
2013-04-07 22:52:48 +0300

    tehtydd

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -2,17 +2,36 @@ package ohtu.verkkokauppa;
 
 public class Kauppa {
 
-    private Varasto varasto;
-    private Pankki pankki;
+    private VarastoRajapinta varasto;
+    private PankkiRajapinta pankki;
     private Ostoskori ostoskori;
-    private Viitegeneraattori viitegeneraattori;
+    private ViitegeneraattoriRajapinta viitegeneraattori;
     private String kaupanTili;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+    public Kauppa(VarastoRajapinta v, PankkiRajapinta p, ViitegeneraattoriRajapinta viiteg) {
+        varasto = v;
+        pankki = p;
+        viitegeneraattori = viiteg;
         kaupanTili = "33333-44455";
+        System.out.println("lol");
+        System.out.println("lol");
+        System.out.println("lol");
+        System.out.println("lol");
+        System.out.println("lol");
+        System.out.println("lol");
+        System.out.println("lol");
+        System.out.println("lol");
+        System.out.println("lol");
+        if (true) {
+            if (true) {
+                System.out.println("sisäkkäinen if");
+            }
+        }
+                for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.println("jeejee");
+            }
+        }
     }
 
     public void aloitaAsiointi() {
@@ -20,13 +39,13 @@ public class Kauppa {
     }
 
     public void poistaKorista(int id) {
-        Tuote t = varasto.haeTuote(id); 
+        Tuote t = varasto.haeTuote(id);
         varasto.palautaVarastoon(t);
     }
 
     public void lisaaKoriin(int id) {
-        if (varasto.saldo(id)>0) {
-            Tuote t = varasto.haeTuote(id);             
+        if (varasto.saldo(id) > 0) {
+            Tuote t = varasto.haeTuote(id);
             ostoskori.lisaa(t);
             varasto.otaVarastosta(t);
         }
@@ -35,8 +54,7 @@ public class Kauppa {
     public boolean tilimaksu(String nimi, String tiliNumero) {
         int viite = viitegeneraattori.uusi();
         int summa = ostoskori.hinta();
-        
+
         return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
     }
-
 }
