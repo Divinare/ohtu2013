e2cc38ea6597096e807794ff0bc411495574a782
Joe <niemijoe@hotmail.com>
2013-04-21 15:21:27 +0300

    lol

--- viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -6,50 +6,49 @@ import java.io.InputStream;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.io.IOUtils;
- 
+
 public class Main {
- 
+
     public static void main(String[] args) throws IOException {
-        String studentNr = "12345678";
-        if ( args.length>0) {
+        String studentNr = "14029938";
+        if (args.length > 0) {
             studentNr = args[0];
         }
-        studentNr = "14029938";
-        String url = "http://ohtustats-2013.herokuapp.com/opiskelija/"+studentNr+".json";
- 
+//        studentNr = "14029938";
+        String url = "http://ohtustats-2013.herokuapp.com/opiskelija/" + studentNr + ".json";
+
         HttpClient client = new HttpClient();
         GetMethod method = new GetMethod(url);
         client.executeMethod(method);
- 
-        InputStream stream =  method.getResponseBodyAsStream();
- 
+
+        InputStream stream = method.getResponseBodyAsStream();
+
         String bodyText = IOUtils.toString(stream);
- 
+
 //        System.out.println("json-muotoinen data:");
-//        System.out.println( bodyText );
- 
+//        System.out.println(bodyText);
+
         Gson mapper = new Gson();
         Palautukset palautukset = mapper.fromJson(bodyText, Palautukset.class);
- 
+
 //        System.out.println("oliot:");
 //        for (Palautus palautus : palautukset.getPalautukset()) {
-//            System.out.println( palautus );
+//            System.out.println(palautus);
 //        }
-        
-        System.out.println("opiskelijanumero: " + palautukset.palautukset.get(0).getOpiskelijanumero() );
-        int indeksi = 1;
-        int yhteensaTehtavia = 0;
-        int aikaaKului = 0;
+        int indeksi = 0;
+        int yhtT = 0;
+        int yhtA = 0;
+        System.out.println("opiskelijanumero " + palautukset.getPalautukset().get(0).getOpiskelijanumero());
+        System.out.println("");
         for (Palautus palautus : palautukset.getPalautukset()) {
-            System.out.println("viikko " + indeksi + ": " + palautus.getTehtavia() + 
-                    " tehtävää " + palautus.getTehtavat() + "     " +
-                    "aikaa kului " + palautus.getTunteja() + " tuntia");
+            System.out.println("viikko " + palautus.getViikko() + ": " + palautus.getTehtavia() + " tehtävää "
+                    + palautus.getTehtavat() + "        aikaa kului " + palautus.getTunteja() + " tuntia");
             indeksi++;
-            yhteensaTehtavia += palautus.getTehtavia();
-            aikaaKului += palautus.getTunteja();
+            yhtT += palautus.getTunteja();
+            yhtA += palautus.getTehtavia();
         }
         System.out.println("");
-        System.out.println("yhteensä " + yhteensaTehtavia + " tehtävää " +
-                aikaaKului + " tuntia");
+        System.out.println("yhteensä " + yhtT + " tehtävää " + yhtA + " tuntia");
+
     }
 }
